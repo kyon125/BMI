02225b36677dff42a34a11e15b9294f26ba4eaa6
m305-34\w401_ <dream7652210@gmail.com>
2018-03-15 17:57:03 +0800

    總之 先測試下

---
+++ bmi/bmi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bmi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int high, weight;
+
+            Console.Write("請輸入您的身高:");
+            high = int.Parse(Console.ReadLine());
+            Console.Write("請輸入您的體重:");
+            weight = int.Parse(Console.ReadLine());
+        }
+    }
+}
