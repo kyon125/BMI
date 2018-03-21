e36a14b71906b01555022ba4a1583cd350278b82
LAPTOP-JAD1OL75\User <dream7652210@gmail.com>
2018-03-21 21:25:23 +0800

    加了While迴圈，讓人能在輸入錯資料時，可以重新輸入

--- bmi/bmi/Program.cs
+++ bmi/bmi/Program.cs
@@ -10,13 +10,62 @@ namespace bmi
     {
         static void Main(string[] args)
         {
-            int high, weight;
+            float High, Weight;
 
-            Console.Write("請輸入您的身高:");
-            high = int.Parse(Console.ReadLine());
-            Console.Write("請輸入您的體重:");
-            weight = int.Parse(Console.ReadLine());
-            Console.Write("請確認您的身高/體重:" + high + weight);
+            while (true)
+            {
+                Console.Write("請輸入您的身高:");
+                High = float.Parse(Console.ReadLine());
+                Console.Write("請輸入您的體重:");
+                Weight = float.Parse(Console.ReadLine());
+                Console.WriteLine("請確認您的身高/體重:" + High + "/" + Weight);
+                Console.WriteLine("資料正確請按Enter鍵，錯誤請按其他鍵。");
+
+                if (Console.ReadKey().Key == ConsoleKey.Enter)//判斷資料的填寫是否正確
+                {
+                    float Bmi;
+                    Bmi = Weight / ((High / 100) * (High / 100));//BMI計算式
+                    Console.WriteLine("您的BMI為:" + Bmi);
+
+                    if (Bmi < 16.5)
+                    {
+                        Console.WriteLine("恭喜您是免役體位。");
+                        Console.Read();
+                    }
+
+
+                    else if (16.5 < Bmi && Bmi < 17)
+                    {
+                        Console.WriteLine("恭喜您是替代役體位。");
+                        Console.Read();
+                    }
+
+                    else if (17 < Bmi && Bmi < 31)
+                    {
+                        Console.WriteLine("抱歉，您是常備役體位......");
+                        Console.Read();
+                    }
+
+                    else if (31 < Bmi && Bmi < 31.5)
+                    {
+                        Console.WriteLine("恭喜您是替代役體位。");
+                        Console.Read();
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("恭喜您是免役體位。");
+                        Console.Read();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine();
+                    continue;//跳回While的開頭
+                }
+
+                break;//跳出迴圈
+            }
         }
     }
 }
